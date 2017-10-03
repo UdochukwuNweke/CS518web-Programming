4dfb35e4495ca4b14826a75d91932d0905a798b8
UdochukwuNweke <unwek001@odu.edu>
2017-10-03 02:10:55 -0400

    upload milestone1 progress

---
+++ Slack/tester.php
@@ -0,0 +1,67 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	include('DB.php');
+
+
+	/*
+	//LOGIN
+	$email ='mater@rsprings.gov';
+	$pass = '@mater';
+	$isLoggedIn = login($email, $pass);
+	var_dump($isLoggedIn);
+	*/
+	
+	
+	/*
+	//POST
+	$channel_id = 6;
+	$parent_id = 6;
+	$content = 'it is right.';
+	$fname= 'Tow';
+	$lname = 'Mater';
+	$user_id = 2;
+
+	$numRows = post($channel_id, $parent_id, $content, $fname, $lname, $user_id);
+	echo 'posted items:' . $numRows;
+	*/
+
+	
+	/*
+	//DELETE POST
+	$post_id = 54;
+	$user_id = 12;
+	$numRows = deletePost($post_id, $user_id);
+	echo 'deleted items:' . $numRows;
+	*/
+
+
+
+
+	/*
+		Todo:
+		
+		login(email, password)
+			change the type that is returned to an array
+			
+			if the user is in the DB
+				array(fname, lname)
+			else
+				array()
+
+		
+		post()
+		- wrap main block of post inside try catch
+		- return the number of rows affected by insert operation
+		- set timezone to EST
+	*/
+
+	
+	
+
+
+
+
+?>
