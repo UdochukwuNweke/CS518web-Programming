b9867e49fa7f99b0d0356ee4c81c540928ec055b
UdochukwuNweke <unwek001@odu.edu>
2017-10-03 14:48:50 -0400

    milestone 1 so far..

---
+++ DB.php
@@ -0,0 +1,127 @@
+<?php
+	date_default_timezone_set('America/New_York');
+
+	$serverName = 'localhost';
+	$userName = 'unweke';
+	$password = 'gaccess123';
+	$dbname = 'milestone1dump';
+
+
+
+	function login($email, $password)
+	{
+		$fnameLname =  array();
+
+		try
+		{
+			$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['dbname']);
+			
+			// Check connection
+			if( $conn -> connect_error ) 
+			{
+				// consider logging error
+	    		echo 'Connection failed: ' . $conn->connect_error;
+			}
+			else
+			{
+				// credit to: http://stackoverflow.com/a/60496
+				$sqlQuery = $conn -> prepare("SELECT fname, lname FROM Users WHERE email=? AND password=?");
+				$sqlQuery -> bind_param('ss', $email, $password);
+				$sqlQuery -> execute();
+
+				$result = $sqlQuery -> get_result();
+				if( $result -> num_rows !== 0 )
+				{
+					$fnameLname = $result -> fetch_assoc();
+				}
+				
+				$sqlQuery -> close();
+				$conn -> close();
+			}
+
+		}
+		catch(Exception $e)
+		{
+			echo 'Message: ' . $e -> getMessage();
+		}
+
+
+		return $fnameLname;
+	}
+
+	
+
+
+	function post ($channel_id, $parent_id, $content, $fname, $lname, $user_id)
+	{
+		$numRows = 0;
+		try
+		{
+			$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['dbname']);
+			$datetime = date("Y-m-d H:i:s");
+			
+			// Check connection
+			if( $conn -> connect_error ) 
+			{
+				// consider logging error
+	    		echo 'Connection failed: ' . $conn->connect_error;
+			}
+			else 
+			{
+				$sqlQuery = $conn -> prepare("INSERT INTO Post( datetime, channel_id, parent_id, content, fname, lname, user_id) VALUE(?, ?, ?, ?, ?, ?, ?)");
+				$sqlQuery -> bind_param('siisssi', $datetime, $channel_id, $parent_id, $content, $fname, $lname, $user_id);	
+				$sqlQuery -> execute();
+
+				$numRows = $conn -> affected_rows;
+
+				$sqlQuery -> close();
+				$conn -> close();
+
+			}
+		}
+		catch(Exception $e){
+			echo 'Message: ' . $e -> getMessage();
+		}
+
+		return $numRows;
+
+	}
+
+	function deletePost($post_id, $user_id)
+	{
+		$numRows = 0;
+
+		try
+		{
+			$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['dbname']);
+			// Check connection
+			if( $conn -> connect_error ) 
+			{
+				// consider logging error
+	    		echo 'Connection failed: ' . $conn->connect_error;
+			}
+			else 
+			{
+				$sqlQuery = $conn -> prepare("DELETE FROM  Post WHERE post_id =? AND user_id = ?");
+				$sqlQuery -> bind_param('ii', $post_id, $user_id);	
+				$sqlQuery -> execute();
+
+				$numRows = $conn -> affected_rows;
+
+				$sqlQuery -> close();
+				$conn -> close();
+
+			}
+		}
+		catch(Exception $e){
+			echo 'Message: ' . $e -> getMessage();
+		}
+		
+
+		return $numRows;
+
+	}
+
+
+?>
+
---
+++ tester.php
@@ -0,0 +1,44 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	include('DB.php');
+
+
+	/*
+	//LOGIN
+	$email ='mater@rsprings.gov';
+	$pass = '@mater';
+	$isLoggedIn = login($email, $pass);
+	var_dump($isLoggedIn);
+	*/
+	
+	
+	/*
+	//POST
+	$channel_id = 6;
+	$parent_id = 6;
+	$content = 'it is right.';
+	$fname= 'Tow';
+	$lname = 'Mater';
+	$user_id = 2;
+
+	$numRows = post($channel_id, $parent_id, $content, $fname, $lname, $user_id);
+	echo 'posted items:' . $numRows;
+	*/
+
+	
+	/*
+	//DELETE POST
+	$post_id = 54;
+	$user_id = 12;
+	$numRows = deletePost($post_id, $user_id);
+	echo 'deleted items:' . $numRows;
+	*/
+	
+
+
+
+
+?>
