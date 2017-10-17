b617777c62cbc3389adbf2b09325511fdafe2d8c
UdochukwuNweke <unwek001@odu.edu>
2017-10-16 22:14:46 -0400

    sql db update

--- DB.php
+++ DB.php
@@ -1,5 +1,6 @@
 <?php
-	date_default_timezone_set('America/New_York');
+//http://www.elated.com/articles/mysql-for-absolute-beginners
+date_default_timezone_set('America/New_York');
 
 	$serverName = 'localhost';
 	#$userName = 'unweke';
@@ -9,123 +10,239 @@
 	$password = 'M0n@rch$';
 	$dbname = 'CS518DB';
 
+/*
+CS518 Tables
+
+	User
+		int user_id
+		string
+			fname
+			lname
+			email
+			password
+
+	Channel
+		channel_id
+		name
+		purpose
+		type
+		creator_id
+
+	Post
+		int
+			post_id
+			user_id
+			channel_id
+			parent_id
+		datetime datetime
+		string content
+
+	Reaction_Type
+		reaction_type_id
+		name
+		emoji
+
+	Reaction
+		reaction_id
+		post_id
+		user_id
+		reaction_type_id
+*/
 
 
-	function login($email, $password)
+/*
+ * Responsible for login action: Check if user supplied email/password pair is in DB
+ *
+ * @param string email
+ * @param string password
+ * @return (array) - user user_id, fname, lname array
+ */
+function login($email, $password)
+{
+	$names = array();
+
+	try
 	{
-		$fnameLname =  array();
+		$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['dbUserName'], $GLOBALS['dbPassword'], $GLOBALS['dbname']);
 
-		try
+		// Check connection
+		if( $conn->connect_error ) 
 		{
-			$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['dbname']);
-			
-			// Check connection
-			if( $conn -> connect_error ) 
-			{
-				// consider logging error
-	    		echo 'Connection failed: ' . $conn->connect_error;
-			}
-			else
-			{
-				// credit to: http://stackoverflow.com/a/60496
-				$sqlQuery = $conn -> prepare("SELECT fname, lname FROM Users WHERE email=? AND password=?");
-				$sqlQuery -> bind_param('ss', $email, $password);
-				$sqlQuery -> execute();
+			// consider logging error
+			echo 'Connection failed: ' . $conn -> connect_error;
+		} 
+		else
+		{
+			$sqlQuery = $conn -> prepare('SELECT user_id, fname, lname from User WHERE email=? AND password=?');
+			$sqlQuery -> bind_param('ss', $email, $password );
+			$sqlQuery -> execute();
 
-				$sqlQuery -> bind_result($fname, $lname);
-				if( $sqlQuery -> fetch() )
-				{
-			        $fnameLname['fname'] = $fname;
-			        $fnameLname['lname'] = $lname;
-				}
-				
-				$sqlQuery -> close();
-				$conn -> close();
+			$sqlQuery -> bind_result($user_id, $fname, $lname);
+			if( $sqlQuery -> fetch() )
+			{
+		        $names['user_id'] = $user_id;
+		        $names['fname'] = $fname;
+		        $names['lname'] = $lname;
 			}
 
+			$sqlQuery -> close();
+			$conn -> close();
 		}
-		catch(Exception $e)
-		{
-			echo 'Message: ' . $e -> getMessage();
-		}
-
-
-		return $fnameLname;
+	}
+	catch(Exception $e) 
+	{
+		echo 'Message: ' . $e -> getMessage();
 	}
 
-	
+	return $names;
+}
 
+/*
+ * Responsible for deleting a post for a user-supplied post_id and user_id pair
+ *
+ * @param int user_id: the identifier for the user who composed the post
+ * @param int post_id: the identifier for the post to be deleted
+ * @return (bool) true - if delete operation successful, false - otherwise
+ */
+function deletePost($post_id, $user_id)
+{
+	$hasRows = false;
 
-	function post ($channel_id, $parent_id, $content, $fname, $lname, $user_id)
+	try
 	{
-		$numRows = 0;
-		try
+		$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['dbUserName'], $GLOBALS['dbPassword'], $GLOBALS['dbname']);
+		// Check connection
+		if( $conn -> connect_error ) 
 		{
-			$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['dbname']);
-			$datetime = date("Y-m-d H:i:s");
-			
-			// Check connection
-			if( $conn -> connect_error ) 
+			// consider logging error
+			echo 'Connection failed: ' . $conn -> connect_error;
+		} 
+		else
+		{
+			$sqlQuery = $conn -> prepare('DELETE FROM Post WHERE post_id=? AND user_id=?');
+			$sqlQuery -> bind_param('ii', $post_id, $user_id);
+
+			$sqlQuery -> execute();
+			if( $conn -> affected_rows !== 0 )
 			{
-				// consider logging error
-	    		echo 'Connection failed: ' . $conn->connect_error;
+				$hasRows = true;
 			}
-			else 
-			{
-				$sqlQuery = $conn -> prepare("INSERT INTO Post( datetime, channel_id, parent_id, content, fname, lname, user_id) VALUE(?, ?, ?, ?, ?, ?, ?)");
-				$sqlQuery -> bind_param('siisssi', $datetime, $channel_id, $parent_id, $content, $fname, $lname, $user_id);	
-				$sqlQuery -> execute();
 
-				$numRows = $conn -> affected_rows;
-
-				$sqlQuery -> close();
-				$conn -> close();
-
-			}
+			$sqlQuery -> close();
+			$conn -> close();
 		}
-		catch(Exception $e){
-			echo 'Message: ' . $e -> getMessage();
-		}
-
-		return $numRows;
-
+	}
+	catch(Exception $e) 
+	{
+		echo 'Message: ' . $e -> getMessage();
 	}
 
-	function deletePost($post_id, $user_id)
+	return $hasRows;
+}
+
+/*
+ * Responsible for adding a post msg to the DB
+ *
+ * @param int user_id: the identifier for the user who composed the post
+ 
+ * @param string fname: 
+ * @param string lname: 
+
+ * @param int channel_id: the identifier for the channel associated with this post
+ * @param int parent_id: the identifier for a post that this post replies (if any). If one doesn't exist set to -1
+ * @param string content: the post content
+ * @return (bool) true - if post successful, false - otherwise
+ */
+function post($user_id, $fname, $lname, $channel_id, $parent_id, $content)
+{
+	$datetime = date('Y-m-d H:i:s');
+	$hasRows = false;
+
+	try
 	{
-		$numRows = 0;
+		$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['dbUserName'], $GLOBALS['dbPassword'], $GLOBALS['dbname']);
 
-		try
+		// Check connection
+		if( $conn -> connect_error ) 
 		{
-			$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['dbname']);
-			// Check connection
-			if( $conn -> connect_error ) 
+			// consider logging error
+			echo 'Connection failed: ' . $conn -> connect_error;
+		} 
+		else
+		{
+			/*
+				int
+					post_id
+					user_id
+					channel_id
+					parent_id
+				datetime datetime
+				string content
+			*/
+
+			$sqlQuery = $conn -> prepare('INSERT INTO  Post (user_id, fname, lname, channel_id, parent_id, datetime, content) VALUES (?, ?, ?, ?, ?, ?, ?)');
+			$sqlQuery -> bind_param(
+				'issiiss', 
+				$user_id,
+				$fname,
+				$lname,
+				$channel_id, 
+				$parent_id,
+				$datetime,
+				$content
+			);
+
+			$sqlQuery -> execute();
+			if( $conn -> affected_rows !== 0 )
 			{
-				// consider logging error
-	    		echo 'Connection failed: ' . $conn->connect_error;
+				$hasRows = true;
 			}
-			else 
-			{
-				$sqlQuery = $conn -> prepare("DELETE FROM  Post WHERE post_id =? AND user_id = ?");
-				$sqlQuery -> bind_param('ii', $post_id, $user_id);	
-				$sqlQuery -> execute();
 
-				$numRows = $conn -> affected_rows;
-
-				$sqlQuery -> close();
-				$conn -> close();
-
-			}
+			$sqlQuery -> close();
+			$conn -> close();
 		}
-		catch(Exception $e){
-			echo 'Message: ' . $e -> getMessage();
-		}
-		
-
-		return $numRows;
-
+	}
+	catch(Exception $e) 
+	{
+		echo 'Message: ' . $e -> getMessage();
 	}
 
+	return $hasRows;
+}
+
+
+/*
+ * Responsible for selecting all records from a user-supplied table
+ *
+ * @param string table: the name of the table to extract all records from
+ * @param string optionalWhereClause: custom defined where clause
+ * @return (array)
+ */
+function genericGetAll($table, $optionalWhereClause='')
+{
+	$table = trim($table);
+	if( strlen($table) == 0 )
+	{
+		return array();
+	}
+
+	$payload = array();
+
+	try
+	{
+		$conn = new mysqli($GLOBALS['serverName'], $GLOBALS['dbUserName'], $GLOBALS['dbPassword'], $GLOBALS['dbname']);
+		$sqlQuery = 'SELECT * FROM ' . $table . ' ' . $optionalWhereClause;
+
+		$result = $conn -> query($sqlQuery);
+		$payload = mysqli_fetch_all ($result, MYSQLI_ASSOC);
+
+	}
+	catch(Exception $e) 
+	{
+		echo 'Message: ' . $e -> getMessage();
+	}
+
+	return $payload;
+}
 
 ?>
-
---
+++ milestone1.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 17, 2017 at 03:12 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `CS518DB`
+--
+CREATE DATABASE IF NOT EXISTS `CS518DB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `CS518DB`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Channel`
+--
+
+CREATE TABLE IF NOT EXISTS `Channel` (
+`channel_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `purpose` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `creator_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Channel`
+--
+
+INSERT INTO `Channel` (`channel_id`, `name`, `purpose`, `type`, `creator_id`) VALUES
+(1, 'General', 'Generic messages', 'PUBLIC', 1),
+(2, 'Random', 'Random messages', 'PUBLIC', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Post`
+--
+
+CREATE TABLE IF NOT EXISTS `Post` (
+`post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `channel_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `content` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Post`
+--
+
+INSERT INTO `Post` (`post_id`, `user_id`, `fname`, `lname`, `channel_id`, `parent_id`, `datetime`, `content`) VALUES
+(1, 1, 'Tom', 'Mater', 1, -1, '2017-09-25 21:25:01', 'hello db'),
+(3, 2, 'Sally', 'Carrera', 1, 1, '2017-09-25 21:30:05', 'reply to hello db'),
+(6, 1, 'Tow', 'Mater', 2, -1, '2017-10-06 16:02:02', 'FIrst client side post at random'),
+(7, 1, 'Tow', 'Mater', 2, -1, '2017-10-06 16:02:57', 'FIrst client side post at random'),
+(8, 1, 'Tow', 'Mater', 1, -1, '2017-10-16 03:56:44', 'new message'),
+(9, 1, 'Tow', 'Mater', 1, -1, '2017-10-16 03:57:25', 'new message'),
+(10, 1, 'Tow', 'Mater', 1, -1, '2017-10-16 03:57:31', 'news msdfjadslkfjdaskfj'),
+(11, 2, 'Sally', 'Carrera', 1, -1, '2017-10-16 03:58:28', 'sally');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE IF NOT EXISTS `User` (
+`user_id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `User`
+--
+
+INSERT INTO `User` (`user_id`, `fname`, `lname`, `email`, `password`) VALUES
+(1, 'Tow', 'Mater', 'mater@rsprings.gov', '@mater'),
+(2, 'Sally', 'Carrera', 'porsche@rsprings.gov', '@sally'),
+(3, 'Doc', 'Hudson', 'hornet@rsprings.gov', '@doc'),
+(4, 'Finn', 'McMissile', 'topsecret@agent.org', '@mcmissile'),
+(5, 'Lightning', 'McQueen', 'kachow@rusteze.com', '@mcqueen'),
+(6, 'Chick', 'Hicks', 'chinga@cars.com', '@chick');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Channel`
+--
+ALTER TABLE `Channel`
+ ADD PRIMARY KEY (`channel_id`);
+
+--
+-- Indexes for table `Post`
+--
+ALTER TABLE `Post`
+ ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+ ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Channel`
+--
+ALTER TABLE `Channel`
+MODIFY `channel_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `Post`
+--
+ALTER TABLE `Post`
+MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
