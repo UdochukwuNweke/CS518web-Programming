a1c2e9f0594fb8b979ab9021a42be0473f1a6f01
UdochukwuNweke <unwek001@odu.edu>
2017-10-16 22:48:57 -0400

    index.php update

--- index.php
+++ index.php
@@ -12,9 +12,8 @@ https://www.w3schools.com/PhP/showphp.asp?filename=demo_form_validation_complete
 <h3> Login </h3>
 <form action="main.php" method="post">
 	E-mail: <input type="text" name="email"><br>
-
 	Password: <input type="password" name="password"><br>
-	<input type="submit" value="Submit">
+	<input type="submit">
 </form>
 
 </body>
