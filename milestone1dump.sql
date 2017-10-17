c0e01bf2ae0ea5743ee24cfbe5404f5acbe7f6ab
UdochukwuNweke <unwek001@odu.edu>
2017-10-16 22:29:59 -0400

    sql db update

--- milestone1dump.sql
+++ milestone1dump.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `CS518DB`
 --
-CREATE DATABASE IF NOT EXISTS `CS518DB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `CS518DB`;
 USE `CS518DB`;
 
 -- --------------------------------------------------------
